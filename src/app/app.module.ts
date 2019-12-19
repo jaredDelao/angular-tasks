f0643f2af6bec4c13b28784137dc5d27dac91f8c
jared <jaredhm07@gmail.com>
2019-12-18 21:06:39 -0600

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PadreComponent } from './components/padre/padre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PadreComponent
   ],
   imports: [
     BrowserModule,
